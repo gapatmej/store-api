6e4895f336a3414e87c33d91dac21eeba0fbe4e7
Andres Peralta <gapatmej@gmail.com>
2024-08-05 14:25:25 -0500

    config axion-release

--- src/test/java/com/aperalta/store/config/TestSecurityConfig.java
+++ src/test/java/com/aperalta/store/config/TestSecurityConfig.java
@@ -1,13 +1,13 @@
 package com.aperalta.store.config;
 
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
+@TestConfiguration
 @EnableWebSecurity
 public class TestSecurityConfig {
 
